f16974e1b05053d3320795e01c02db9e76079d57
jimflores <jflores@launchcode.org>
2019-10-31 12:05:52 -0500

    Added boilerplate code to Main.

--- src/org/launchcode/TechJobsOO/Main.java
+++ src/org/launchcode/TechJobsOO/Main.java
@@ -1,6 +1,20 @@
 package org.launchcode.TechJobsOO;
 
+import java.util.ArrayList;
+
 public class Main {
 
+//    public static void main(String[] args) {
+//        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+//        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+//
+//        ArrayList<Job> jobs = new ArrayList<>();
+//        jobs.add(job1);
+//        jobs.add(job2);
+//
+//        for (Job job : jobs){
+//            System.out.println(job);
+//        }
+//    }
 
 }
