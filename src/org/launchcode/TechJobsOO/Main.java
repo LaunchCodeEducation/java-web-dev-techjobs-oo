54e471546bfe3f217623cdcba9de5b1a0c5ad8a5
jimflores <jflores@launchcode.org>
2019-10-31 12:20:20 -0500

    Added commented-out code in Main for students to use to validate their completed assignment.

--- src/org/launchcode/TechJobsOO/Main.java
+++ src/org/launchcode/TechJobsOO/Main.java
@@ -7,6 +7,7 @@ public class Main {
 //    public static void main(String[] args) {
 //        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 //        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+//        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
 //
 //        ArrayList<Job> jobs = new ArrayList<>();
 //        jobs.add(job1);
