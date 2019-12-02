519e14b15c53ace9aed88464b515dc7684333d5f
James <jamesr817@gmail.com>
2019-12-02 06:29:56 -0600

    Finally on 5 of 6 parts of assignment. Should have full completetion within the next day and a half. Marked to main questions to ask as TODOs. Willtry to complete part 5 before class.

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs_oo.Tests;
 
+import org.junit.After;
 import org.junit.Before;
 
 import org.junit.Test;
@@ -7,7 +8,7 @@ import org.launchcode.techjobs_oo.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-
+//TODO ask if the "extends" keyword was even necessary
 public class JobTest extends Job {
     Job test_JobA;
     Job test_JobsB;
@@ -25,13 +26,17 @@ public class JobTest extends Job {
                 new PositionType("Quality Control"), new CoreCompetency("Persistence"));
 
     }
+    @After
+    public void done() {
+        System.out.println("Test passed...\n Test completed!");
+    }
 
     @Test()
     public void testSettingJobId() {
-        assertEquals(test_JobA.getId(), test_JobsB.getId(), 1);
+        assertEquals(test_JobA.getId(), test_JobsB.getId(), 1);//TODO Take out before merging to master
         assertThat("Job IDs are equal", test_JobA.getId(),is(not(test_JobsB.getId())));
-        System.out.println(test_JobA.getId());
-        System.out.println(test_JobsB.getId());
+        //System.out.println(test_JobA.getId());//TODO Take out before merging to master
+        //System.out.println(test_JobsB.getId());//TODO Take out before merging to master
     }
 
     @Test
@@ -44,19 +49,26 @@ public class JobTest extends Job {
         assertTrue(demoJobA.getPositionType() instanceof PositionType);
         assertTrue(demoJobA.getCoreCompetency() instanceof CoreCompetency);
 
-
+        //Testing for Field Values
+        assertThat("Product Tester", is(demoJobA.getName()));
+        assertThat("ACME", is(demoJobA.getEmployer().getValue()));
+        assertThat("Desert", is(demoJobA.getLocation().getValue()));
+        assertThat("Quality Control", is(demoJobA.getPositionType().getValue()));
+        assertThat("Persistence", is(demoJobA.getCoreCompetency().getValue()));
 
     }
 
     @Test
-    //Why does this test even exist, isn't it pretty much the same as "testSettingJobId()"????
+    //TODO Why does this test even exist, isn't it pretty much the same as "testSettingJobId()"????
     public void testEquals() {
         int demoA_ID = demoJobA.getId();
         int demoB_ID = demoJobB.getId();
         assertThat("Should not be equal",demoA_ID,is(not(demoB_ID)));
 
+        //TODO Take out before merging to master. Ask questions first
         //*Just want clarification on the three test in this block*
         //assertFalse(!(demoA_ID != demoB_ID));
         //assertFalse(demoA_ID == demoB_ID);
     }
+
 }
